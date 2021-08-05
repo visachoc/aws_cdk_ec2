b12db09eff77c1aacfe791f14f1e35deb0776c54
Sundaramchockalingam <913933@cognizant.com>
2021-08-05 19:57:04 +0530

    my first cdk ec2 instance

--- aws_cdk_ec2/aws_cdk_ec2_stack.py
+++ aws_cdk_ec2/aws_cdk_ec2_stack.py
@@ -1,15 +1,35 @@
-from aws_cdk import core as cdk
+from aws_cdk.aws_s3_assets import Asset
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    core    
+)
+class AwsCdkEc2Stack(core.Stack):
 
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-class AwsCdkEc2Stack(cdk.Stack):
+        # VPC
+        vpc = ec2.Vpc(self, "VPC",
+            nat_gateways=0,
+            subnet_configuration=[ec2.SubnetConfiguration(name="public",subnet_type=ec2.SubnetType.PUBLIC)]
+            )
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        # AMI 
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
 
-        # The code that defines your stack goes here
+        # Instance Role and SSM Managed Policy
+      
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=amzn_linux,
+            vpc = vpc,
+           
+        )
